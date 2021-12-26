3f6a7fa695dc2cb3b7f49341c1fdfffbf6aea2c0
kerness <maciej.bak260@gmail.com>
2021-12-26 20:36:55 +0100

    footer in menu fixed

--- js/template.js
+++ js/template.js
@@ -5,7 +5,7 @@ function handleMenuButton() {
     if(overlay.style.display == "none") {
         overlay.style.display = "block";
         document.getElementById("headContainer").style.backgroundColor = "#3D3939";
-        document.getElementById("footerContainer").style.backgroundColor = "#3D3939";
+        //document.getElementById("footerContainer").style.backgroundColor = "#3D3939";
         document.getElementById("topBar").style.borderBottom = "white solid 3px";
         document.getElementById("bottomBar").style.borderTop = "white solid 3px";
         var texts = document.getElementsByClassName("text");
@@ -16,7 +16,7 @@ function handleMenuButton() {
     } else {
         overlay.style.display = "none";
         document.getElementById("headContainer").style.backgroundColor = "";
-        document.getElementById("footerContainer").style.backgroundColor = "";
+        //document.getElementById("footerContainer").style.backgroundColor = "";
         document.getElementById("topBar").style.borderBottom = "";
         document.getElementById("bottomBar").style.borderTop = "";
         var texts = document.getElementsByClassName("text");
